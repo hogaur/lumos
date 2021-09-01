03e9143951f07b5bd85792966733e5d084a8ee4c
Hariom Gaur <hariomgaur@Harioms-MacBook-Pro.local>
2021-09-01 12:34:12 +0530

    Adds homebrew installation

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ Workspace setup for Developers using Mac.
 Assumes
 - Ruby is already installed on the system and available
 - xcode or respective dev tools are already installed.
+
+What happens when you run `ruby runner.rb`
+- Creates workspace dir 
+- Install homebrew
+
+What isn't automated
+- Install xcode command line tools using `xcode-select --install` & verify using `xcode-select -p`
+- 
--- runner.rb
+++ runner.rb
@@ -1,8 +1,17 @@
-#!/bin/ruby
-
 def create_workspace
   system('mkdir -p ~/workspace')
 end
 
+def install_homebrew
+  system('/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"')
+end
+
+def on_success(message)
+  p message if $?.success?
+end
+
 create_workspace
-p "created_workspace"
+on_success "created_workspace"
+
+install_homebrew
+on_success "installed homebrew"
